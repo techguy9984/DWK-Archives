05ab1a04e128a2ba719b101ca42ebd842db21726
Will <wdavies973@gmail.com>
2016-04-27 14:12:49 -0500

    Updated slider class

--- Stayfrosty 2D Game Engine/src/com/cpjd/stayfrosty/menu/Slider.java
+++ Stayfrosty 2D Game Engine/src/com/cpjd/stayfrosty/menu/Slider.java
@@ -4,17 +4,28 @@ import java.awt.Graphics2D;
 
 public class Slider {
 	
+	// The slider
 	private int x;
 	private int y;
 	private int width;
 	
-	public Slider(int width) {
+	// The button
+	private int count;
+	private final int size = 40; // Diameter of the button
+	
+	// Min and max
+	private int min;
+	private int max;
+	private int ratio; // Ratio of pxs to counts
+	
+	public Slider(int x, int y, int width) {
 		this.width = width;
+		this.x = x;
+		this.y = y;
+		
+		min = x;
+		max = x + width;
 	}
-	
-	
-	
-	
 	
 	public void update() {
 		
@@ -22,6 +33,9 @@ public class Slider {
 	}
 	
 	public void draw(Graphics2D g) {
+		g.setColor(GameStateManager.hud);
+		g.fillRect(x,y,width,40);
+		
 		
 	}
 	
