b0b30d54a987d394118542a087d038f8e0a43944
Will <wdavies973@gmail.com>
2016-04-27 14:21:39 -0500

    Added a new color wheel

---
+++ Stayfrosty 2D Game Engine/src/com/cpjd/stayfrosty/gamestate/GUI.java
@@ -0,0 +1,32 @@
+public class GUI {
+  
+  public static Color background;
+  public static Color text;
+  public static Color boldText; // A slight variation of text
+  
+  public static void update() {
+    text = new Color(255 - background.getRed(), 255 - background.getGreen(), 255 - background.getBlue());
+    
+    // Calculate a slight inversion
+    int r = text.getRed();
+    int g = text.getGreen();
+    int b = text.getBlue();
+    
+    r += 30; g += 30; b += 3-;
+    if(r > 255) {
+      int diff = r - 255;
+      r = diff;
+    }
+    if(g > 255) {
+      int diff = g - 255;
+      g = diff;
+    }
+    if(g > 255) {
+      int diff = g - 255;
+      g = diff;
+    }
+    
+    boldText = new Color(r,g,b);
+  }
+  
+}
